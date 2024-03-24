c9d77ca749cde82626daa3abbe3a96b190cfc803
Weslley Addson <wesquel@hotmail.com>
2024-03-23 23:36:19 -0300

    Add get All Bets in controller.

--- src/main/java/com/addsonweslley/loteriasapi/controllers/BetController.java
+++ src/main/java/com/addsonweslley/loteriasapi/controllers/BetController.java
@@ -2,6 +2,8 @@ package com.addsonweslley.loteriasapi.controllers;
 
 import com.addsonweslley.loteriasapi.dtos.Bet.BetRequest;
 import com.addsonweslley.loteriasapi.services.BetService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,22 @@ public class BetController {
 
     public BetController(BetService betService) {
         this.betService = betService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> getAllBets(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "12") Integer size
+    ){
+        Pageable pageable = PageRequest.of(page, size);
+        return betService.getAllBets(pageable);
+    }
+
+    @GetMapping("/{UUID}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> getBet(@PathVariable("UUID") String uuid){
+        return betService.getBet(uuid);
     }
 
     @PostMapping("/create")
--- src/main/java/com/addsonweslley/loteriasapi/services/BetService.java
+++ src/main/java/com/addsonweslley/loteriasapi/services/BetService.java
@@ -5,9 +5,11 @@ import com.addsonweslley.loteriasapi.dtos.Bet.BetResponse;
 import com.addsonweslley.loteriasapi.models.Bet;
 import com.addsonweslley.loteriasapi.models.Ticket;
 import com.addsonweslley.loteriasapi.repository.BetRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -22,17 +24,43 @@ public class BetService {
     }
 
     public BetResponse betToResponse(Bet bet){
+        System.out.println(bet.getId());
+        System.out.println(bet.getMaxNumbersByUsers());
         return new BetResponse(bet.getId(), bet.getMaxNumbersByUsers(), bet.getToltalNumbers());
     }
 
     public ResponseEntity<BetResponse> createBet(BetRequest betRequest) {
         Bet bet = new Bet();
-        bet.setId(UUID.randomUUID());
+        UUID uuid = UUID.randomUUID();
+        bet.setId(uuid);
         bet.setToltalNumbers(betRequest.toltalNumbers());
         bet.setMaxNumbersByUsers(betRequest.maxNumbersByUsers());
         List<Ticket> ticketList = new ArrayList<>();
         bet.setTicketList(ticketList);
         betRepository.save(bet);
+        return ResponseEntity.created(URI.create("/api/bet/get/"+uuid)).body(betToResponse(bet));
+    }
+
+    public ResponseEntity<?> getAllBets(Pageable pageable){
+        var betPage = betRepository.findAll(pageable);
+        var pageResponseBet = betPage.map(this::betToResponse);
+        System.out.println(pageResponseBet);
+        return ResponseEntity.ok().body(pageResponseBet);
+    }
+
+    public ResponseEntity<?> getBet(String uuidString) {
+        UUID uuid = UUID.fromString(uuidString);
+        Bet bet = betRepository.getReferenceById(uuid);
         return ResponseEntity.ok().body(betToResponse(bet));
+    }
+
+    // Método para converter a representação hexadecimal para UUID
+    private String hexToStringUuid(String hex) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hex.length(); i += 2) {
+            String str = hex.substring(i, i + 2);
+            sb.append((char) Integer.parseInt(str, 16));
+        }
+        return sb.toString();
     }
 }
